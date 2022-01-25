70d8bcb65b3ab70ced91fd74308dbcd4585169c7
ericjlie <ericjlie@gmail.com>
2022-01-24 18:05:28 -0800

    feat: init commit w/ webpack

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  entry: './client/src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'client/dist'),
+    filename: 'bundle.js',
+  },
+  mode: 'development',
+  module: {
+
+    rules: [
+      {
+        test: /\.m?(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', "@babel/preset-react"]
+          }
+        }
+      }
+    ]
+  }
+}
